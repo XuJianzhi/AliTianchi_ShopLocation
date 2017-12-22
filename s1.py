0baeab68fd10c80b27e2a244b31372101fdc0280
XuJianzhi <30584453+XuJianzhi@users.noreply.github.com>
2017-12-22 14:59:16 +0800

    Update s1.py

--- s1.py
+++ s1.py
@@ -1,3 +1,5 @@
+
+
 import pandas as pd
 import numpy as np
 import sklearn as sk
@@ -9,46 +11,51 @@ import gc
 # 提取
 way1='C:/Users/Administrator/Desktop/ali/data/1_use/1_ccf_first_round_shop_info.csv'
 way2='C:/Users/Administrator/Desktop/ali/data/1_use/2_ccf_first_round_user_shop_behavior.csv'
-way3='C:/Users/Administrator/Desktop/ali/data/1_use/3_evaluation_public.csv'
+#way3='C:/Users/Administrator/Desktop/ali/data/1_use/3_evaluation_public.csv'
 
 data1=pd.read_csv(way1)
 data2=pd.read_csv(way2)
-data3=pd.read_csv(way3)
+#data3=pd.read_csv(way3)
 
 data=pd.merge(data1,data2,on='shop_id',suffixes=('_real','_variable'))
-
-
 
 #x=data[['longitude_variable','latitude_variable']]
 #y=data['shop_id']
 #z=data['mall_id']
 w=data['wifi_infos']
 
-#标准化
-#scale=StandardScaler()
-#xx=pd.DataFrame(scale.fit_transform(x))
-df_wifi=pd.DataFrame()
-for i in w:
+mall_list=data['mall_id'].unique()
+#number_loop=0
+'''
+for j in mall_list:
+	df_wifi=pd.DataFrame()
+	for i in w[data['mall_id']==j]:
+		s1=i.split(';')
+		s2=[np.nan]*len(s1)
+		for j in range(len(s1)):
+			s2[j]=s1[j].split('|')
+		s3=pd.DataFrame(s2).set_index(0).drop(2,axis=1).T
+		df_wifi=df_wifi.append(s3,ignore_index=True)
+		print(df_wifi)
+		#gc.collect()
+'''
+for j in mall_list:
+	df_wifi=pd.DataFrame()
+	for i in w[data['mall_id']==j]:
+		s1=pd.Series(i.split(';'))
+		s2=s1.apply(lambda x:pd.Series(x.split('|'))).set_index(0).T
+		s3=s2.drop(2)
+		df_wifi=df_wifi.append(s3,ignore_index=True)
+		print(df_wifi)
+	df_wifi.index=w[data['mall_id']==j].index
+	
 
-	s1=i.split(';')
-	s2=[np.nan]*len(s1)
-	for j in range(len(s1)):
-		s2[j]=s1[j].split('|')
-	s3=pd.DataFrame(s2).set_index(0).drop(2,axis=1).T
-	df_wifi=df_wifi.append(s3)
-	print(df_wifi)
-	gc.collect()
 
 
 
 
 
-
-
-
-
-
-
+'''
 #储存数据
 way_write='C:/Users/Administrator/Desktop/ali/data/3_tempt/'
 
@@ -58,3 +65,6 @@ z.to_csv(way_write+'z.csv')
 x.to_csv(way_write+'w.csv')
 
 joblib.dump(scale, way_write+'scale_1.model')
+'''
+
+
